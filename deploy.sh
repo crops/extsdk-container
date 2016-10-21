5ffda68882a8fb6c30a7f797128a1e7d16b4f2a1
Randy Witt <randy.e.witt@linux.intel.com>
2016-10-21 16:40:46 -0700

    deploy.sh: Only push if on master branch
    The only branch's changes who should cause a push to dockerhub is
    master.
    
    Signed-off-by: Randy Witt <randy.e.witt@linux.intel.com>

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ set -e
 
 # Don't deploy on pull requests because it could just be junk code that won't
 # get merged
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then
     docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     docker push ${REPO}
 else
