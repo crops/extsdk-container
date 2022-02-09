42444b1803fc6ca904ed0ef473d30d180a4008ed
Tim Orling <tim.orling@konsulko.com>
2022-02-09 15:57:06 -0800

    deploy.sh: fix typo
    Add missing ']'
    
    Signed-off-by: Tim Orling <tim.orling@konsulko.com>

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ set -e
 
 # Don't deploy on pull requests because it could just be junk code that won't
 # get merged
-if ([ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "workflow_dispatch" ] || [ "${GITHUB_EVENT_NAME}" = "schedule" ) && [ "${GITHUB_REF}" = "refs/heads/master" ]; then
+if ([ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "workflow_dispatch" ] || [ "${GITHUB_EVENT_NAME}" = "schedule" ]) && [ "${GITHUB_REF}" = "refs/heads/master" ]; then
     echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
     docker push ${REPO}:latest
 else
